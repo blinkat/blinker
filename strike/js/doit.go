e998e18302e32381eeea308689c1e83b4e016b60
blinker <ltysime@126.com>
2015-06-09 14:18:42 +0800

    combine

--- strike/js/doit.go
+++ strike/js/doit.go
@@ -1,8 +1,10 @@
 package js
 
 import (
+	"bytes"
 	p "github.com/blinkat/blinker/strike/js/parser"
 	"github.com/blinkat/blinker/strike/js/process"
+	"io/ioutil"
 )
 
 func Strike(text string) string {
@@ -12,4 +14,17 @@ func Strike(text string) string {
 	ast = process.MangleAst(ast, wk)
 	ast = process.Squeeze(ast, wk)
 	return process.GenCode(ast, wk)
+}
+
+// combine some js file
+func Combine(paths []string) string {
+	var ret bytes.Buffer
+	for v, _ := range paths {
+		text, err := ioutil.ReadFile(v)
+		if err == nil {
+			ret.Write(text)
+			ret.WriteRune('\n')
+		}
+	}
+	return string(ret.Bytes())
 }
